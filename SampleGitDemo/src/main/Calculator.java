4f1ab2758c0ec674d9299ad3e5c630d1c1db6894
HP <HP@Lijitha-Varma>
2021-03-22 18:08:49 +0530

    added add function

--- SampleGitDemo/src/main/Calculator.java
+++ SampleGitDemo/src/main/Calculator.java
@@ -4,7 +4,9 @@ public class Calculator {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-
-	}
+}
+	public static void add(int num1,int num2){
+	      System.out.println(num1+num2);
+	  }
 
 }
