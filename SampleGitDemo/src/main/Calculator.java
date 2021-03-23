1dab1752518080b544a7bdc3c0d4bc3d18895eef
HP <HP@Lijitha-Varma>
2021-03-23 10:37:41 +0530

    added cube function

--- SampleGitDemo/src/main/Calculator.java
+++ SampleGitDemo/src/main/Calculator.java
@@ -21,5 +21,8 @@ public class Calculator {
 	public static void mod(int num1,int num2){
 	      System.out.println(num1%num2);
 	  }
+	public static void cube(int num1){
+	      System.out.println(num1*num1*num1);
+	  }
 	
 }
