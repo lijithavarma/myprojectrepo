2c3e46b35ef117435cf6585e8584359388148054
HP <HP@Lijitha-Varma>
2021-03-23 10:56:49 +0530

    added master func1

--- SampleGitDemo/src/main/Calculator.java
+++ SampleGitDemo/src/main/Calculator.java
@@ -24,5 +24,9 @@ public class Calculator {
 	public static void cube(int num1){
 	      System.out.println(num1*num1*num1);
 	  }
+	public static void masterfunc1(int num1){
+	      System.out.println(num1*num1*num1);
+	  }
+	
 	
 }
